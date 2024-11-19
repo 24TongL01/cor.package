0bccd0b4c69b565b399e86903e15eb44fcbce632
liutong <liutong.csu@gmail.com>
2024-11-19 11:15:50 -0500

    update

--- R/cor.package.R
+++ R/cor.package.R
@@ -1,21 +1,30 @@
 #' cor.package: A Package for Correlation Analysis
 #'
-#' This package provides functions to calculate correlation efficiently.
-#' It includes tools for handling missing values, computing Pearson and Spearman correlations,
-#' and visualizing correlation matrices.
+#' The cor.package provides efficient and flexible tools for:
+
+#'Calculating correlation coefficients using customizable methods (cor_new).
+#'Handling missing values in datasets (handle_na).
+#'Computing and visualizing correlation matrices for high-dimensional data (efficient_corr_matrix).
+#'Calculating correlation coefficients more efficiently using Rcpp (cor_new_rcpp_wrapper).
+#'
 #'
 #' ## Main Functions
-#' - `cor_function()`: Computes the correlation between two numeric vectors.
+#' - `cor_new()`: Computes the correlation between two numeric vectors.
 #' - `handle_na()`: Handles missing values in numeric datasets.
-#'
+#' - `efficient_corr_matrix()`: Computes the correlation at the high dimensional level.
+#' - `cor_new_rcpp_wrapper()`: Realizing cor_new() using C++.
 #' ## Example
-#' ```
+#'
 #' library(cor.package)
-#' x <- c(1, 2, 3, NA, 5)
-#' y <- c(5, 4, NA, 2, 1)
-#' handle_na(x, y, method = "omit")
-#' cor_function(x, y)
-#' ```
+#' x = c(1, 2, 3, NA, 5)
+#' y = c(2, 4, NA, 8, 10)
+#' #Pearson correlation with mean imputation
+#' cor_new(x, y, method = "pearson", na_handle = "mean")
+#' #Spearman correlation with complete case handling
+#' cor_new(x, y, method = "spearman", na_handle = "complete")
+#' #Kendall correlation
+#' cor_new(x, y, method = "kendall")
+#'
 #'
 #' @keywords internal
 "_PACKAGE"
